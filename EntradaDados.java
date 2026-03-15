335678c248b59ea9123d16f45c778ced72e8008a
Renan Martins <renanmartynzf@gmail.com>
2026-03-14 23:22:14 -0300

    Second java file

---
+++ EntradaDados.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class EntradaDados {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String NomeProd = null;
+        double ValorProd = 0;
+        double TotalCompra = 0;
+        int ItensCompra = 0;
+        char opc;
+        do {
+        System.out.print("\nNome do Produto: ");
+        NomeProd = scanner.next();
+        scanner.nextLine();
+        System.out.print("\nPreço: ");
+        while(!scanner.hasNextDouble() || scanner.nextDouble()<=0){
+        System.out.print("\nErro: valor inválido!");
+        System.out.print("\nPreço: ");
+        scanner.next();
+        }
+        ValorProd = scanner.nextDouble();
+        System.out.print("\nRegistrado: " + NomeProd + " - R$" + ValorProd);
+        System.out.print("\nDeseja continuar? (S/N): ");
+        TotalCompra += ValorProd;
+        ItensCompra ++;
+        opc = scanner.next().charAt(0);
+        scanner.nextLine();
+        }
+        while (opc == 's' || opc == 'S');
+        System.out.print("Total da compra: " + TotalCompra + " | Quantidade de Itens: " + ItensCompra);
+        scanner.close();
+    }
+}
